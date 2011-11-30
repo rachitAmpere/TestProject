665999b0f010a0526fa436021d22530e853b321d
Rachit <rachit@amperesoftware.com>
2011-11-30 18:40:20 +0530

    dev24

--- About.aspx.cs
+++ About.aspx.cs
@@ -16,5 +16,6 @@ public partial class About : System.Web.UI.Page
         string test3 = string.Empty;
         string test4 = string.Empty;
         string test5 = string.Empty;
+        string dev24 = string.Empty; 
     }
 }
