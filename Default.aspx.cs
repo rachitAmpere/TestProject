81ace1c892952be20ad023fe1e18d7f1621fe0d8
Rachit <rachit@amperesoftware.com>
2011-11-30 18:19:12 +0530

    Merge branch 'master' of github.com:rachitAmpere/TestProject
    Conflicts:
    	Default.aspx.cs
    	Default.aspx.cs.orig

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -11,20 +11,6 @@ public partial class _Default : System.Web.UI.Page
     {
         string STRDEV2 = string.Empty;
 
-        string test2 = string.Empty;
-        string test3 = string.Empty;
-        string test4 = string.Empty;
-        string test5 = string.Empty;
-        string test6 = string.Empty;
-        string test7 = string.Empty;
-        string dev1 = string.Empty; 
-        string dev2 = string.Empty;
-        string dev12 = string.Empty;
-        string dev13 = string.Empty;
-        string dev14 = string.Empty;
-        string dev15 = string.Empty;
-        string dev26 = string.Empty;
-        string dev27 = string.Empty;
-        string dev28 = string.Empty; 
+       
     }
 }
