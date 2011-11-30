fc415fa7a21a0d1c587dcd3ee446cc0b0bb805cb
Rachit <rachit@amperesoftware.com>
2011-11-30 16:44:56 +0530

    p;lk

---
+++ Default.aspx.cs.BACKUP.3280.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string STRDEV2 = string.Empty;
+
+        string test2 = string.Empty;
+        string test3 = string.Empty;
+        string test4 = string.Empty;
+        string test5 = string.Empty;
+        string test6 = string.Empty;
+        string test7 = string.Empty;
+<<<<<<< HEAD
+        string dev1 = string.Empty; 
+=======
+
+        string dev2 = string.Empty;
+>>>>>>> 4715724ad8130189f0a9df58ee0402acad480bdc
+    }
+}
