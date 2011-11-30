1b1e74b81a0f5bb554c7510fcea8bf818b13dcd0
Rachit <rachit@amperesoftware.com>
2011-11-30 18:31:05 +0530

    dev12

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,6 +9,7 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        string dev21 = string.Empty; 
+        string dev21 = string.Empty;
+        string dev12 = string.Empty; 
     }
 }
