2a85305361267b3b5bc1f707f75e86b67d7908f9
Rachit <rachit@amperesoftware.com>
2011-12-01 16:06:23 +0530

    str27

--- About.aspx.cs
+++ About.aspx.cs
@@ -30,6 +30,7 @@ public partial class About : System.Web.UI.Page
         string str24 = "";
         string str25 = "";
         string str26 = "";
+        string str27 = "";
     }
     protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
     {
